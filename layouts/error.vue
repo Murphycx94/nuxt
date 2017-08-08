d36f9dc2b1f96ad1fd1a3fc1d4094bbeaa049d43
储笑 <shmilyclxy@163.com>
2017-08-08 18:03:44 +0800

    fixed

---
+++ layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="container">
+		<h1>页面不存在</h1>
+		<h1>应用发生错误异常</h1>
+		<nuxt-link to="/">首 页</nuxt-link>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+<section class="container">
+	主页面
+</section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="sass">
+
+</style>
